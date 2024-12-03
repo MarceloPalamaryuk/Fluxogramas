575bd0b7112b6fa69b09ea25f1968782c9a203f8
MarceloPalamaryuk <a14721@oficina.pt>
2024-12-03 11:45:43 +0000

    Add files via upload

--- ex22_arrays_multi.cpp
+++ ex22_arrays_multi.cpp
@@ -5,20 +5,20 @@ using namespace std;
 const int numColunas = 9;
 const int numLinhas = 6;
 string tabuleiro[numColunas][numLinhas];
-string jogador1 = "X", jogador2 = "O";
+string jogador = "X";
 
 // Os espaços vazios são representados como "*".
 void iniciarTabela() {
     for (int i = 0; i<numLinhas; i++) {
         for (int j = 0; j<numColunas; j++) {
-                tabuleiro[i][j] = "~";
+                tabuleiro[i][j] = "+";
         }
     }
 }
 
 //no inicio de cada rodada esta tabela vai aparecer.
 void mostraTabela() {
-    cout << "  1 2 3 4 5 6 7 8 9\n";
+    cout << " 1 2 3 4 5 6 7 8 9\n";
     for (int i = 0; i<numLinhas; i++) {
         for (int j = 0; j<numColunas; j++) {
             cout << " " << tabuleiro[i][j];
@@ -30,10 +30,33 @@ void mostraTabela() {
 void recomecarJogo() {
     for (int i = 0; i<numLinhas; i++) {
         for (int j = 0; j<numColunas; j++) {
-                tabuleiro[i][j] = "~";
+                tabuleiro[i][j] = "+";
         }
     }
+};
+
+void jogar() {
+    int res, aux = 0;
+    cout << "Onde quere jogar?";
+    cin >> res;
+    if (tabuleiro[aux][res-1] == "+") {
+        tabuleiro[aux][res-1] = jogador;
+    } else {
+        for (int i = 0; i<numLinhas; i++) {
+            if (tabuleiro[i][res-1] != "+") {
+                aux = aux+1;
+            }
+        }
+        tabuleiro[aux][res-1] = jogador;
+    }
+
+
+    if (jogador == "X") {
+        jogador = "O";
+    } else if (jogador == "O") {jogador = "X";}
 }
+
+
 bool tabuleiroCheio() {
     return false;
 }
@@ -49,7 +72,11 @@ int main() {
 
     iniciarTabela();
 
+    do {
     mostraTabela();
+
+    jogar();
+    } while (op == 0);
 
     return 0;
 }
